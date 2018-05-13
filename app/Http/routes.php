60a73f468b83814a9bd88e155efbea1e1e8875b5
Mohammad <you@example.com>
2018-05-14 01:46:57 +0300

    Adding my first project on Git-Hub

---
+++ app/Http/routes.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// // The parameters will be pass to cluser function in order not by name
+// Route::get('/posts/{id}/{name}', function ($ids, $name) {
+// 	return 'About page '.$ids.' '.$name;
+// });
+
+// // Two ways to name as routes
+// Route::get('category/subCategory/terms/PHP', array('as' => 'parent.child', function () {
+// 	$url = route('uu');
+// 	return $url;
+// }));
+
+// Route::get('/url', function (){
+// 	return route('parent.child');
+// })->name('uu');
+
+Route::get('/posts/{id}', 'PostsController@index');
